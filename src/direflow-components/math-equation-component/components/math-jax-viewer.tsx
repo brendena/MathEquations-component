65c575416d2b87d747896450f8f7f5bf68f99603
brendena <badamczak@creativemicro.com>
2022-05-16 18:45:32 -0400

    the drag and drop mostly works

--- src/direflow-components/math-equation-component/components/math-jax-viewer.tsx
+++ src/direflow-components/math-equation-component/components/math-jax-viewer.tsx
@@ -1,98 +1,24 @@
 import React, {useRef} from 'react';
 import { MathJaxContext, MathJax } from 'better-react-mathjax';
 import { AppContext } from "../context";
-import {Canvg} from "canvg"
-import * as pngMeta from "@nashiinc/png-metadata/index.js"
+import { useMathJaxImage } from '../handles/useMathJaxImage';
 
 
-interface ClipboardItem {
-  readonly types: string[];
-  readonly presentationStyle: "unspecified" | "inline" | "attachment";
-  getType(): Promise<Blob>;
-}
-
-interface ClipboardItemData {
-  [mimeType: string]: any;
-}
-
-declare var ClipboardItem: {
-  prototype: ClipboardItem;
-  new (itemData: ClipboardItemData): ClipboardItem;
-};
 
 const MathJaxViewer: React.FC = () => {
   const { state  } = React.useContext(AppContext);
-  let ref = useRef(null);
-  let canvasRef = useRef(null);
-
+  const {mathJaxConRef,canvasRef,addCanvasToClipboard,onDrag,onMouseDown } = useMathJaxImage();
 
   const error = (err:any) =>{ console.log("############Error"); console.log(err)}
 
 
-  const  imageChanged =  ()=>{
-    let refTmp :any = ref.current;
-
-    if(refTmp != null)
-    {
-      let svgData = refTmp.getElementsByTagName('mjx-container')[0];
-      svgData.removeChild(svgData.getElementsByTagName("mjx-assistive-mml")[0]);
-      svgData = svgData.innerHTML;
-
-
-      let canvas = refTmp.getElementsByTagName('canvas')[0];
-      let ctx = canvas.getContext('2d');
-
-      let v = Canvg.fromString(ctx,svgData)
-      v.start();
-      
-
-    }
-  }
-
-  let addClipBoard = async ()=>{
-    //navigator.clipboard.writeText('This text is now in the clipboard');
-    let canvas :any = canvasRef.current 
-    if(canvas != null)
-    {
-      let test :any = navigator.clipboard;
-      const canvasWait = async ()=>{
-        return new Promise(resolve  =>{ 
-          canvas.toBlob((blob :any)=>{resolve(blob);}); //converts it to png
-        });
-      }
-
-      //get canvas into png and into Uint8Array
-      let blobb :any = await canvasWait();
-      const blobArray = await blobb.arrayBuffer();
-      const blob8 = new Uint8Array(blobArray);
-
-      //get the metadata then 
-      let metaData = pngMeta.readMetadata(blob8)
-      metaData["tEXt"] = {"test":"shit"}
-      let newBlob = pngMeta.writeMetadata(blob8,metaData);
-      
-      //add to clipboard
-      const backToBlob = new Blob([newBlob],{type: "image/png"});
-      test.write([
-        new ClipboardItem({
-          "image/png": backToBlob
-        })
-      ]); 
-    }    
-  }
-
-  let dragStart= (event : React.DragEvent<HTMLDivElement> )=>{
-    console.log("drag started");
-    event.dataTransfer.setData("text", "testing this out");
-  }
-
   let text = state.EquationProps.text; 
   if(text === "") text=" ";
   return (
-    <div id="mathJaxViewer" ref={ref} draggable={true} onDragStart={dragStart}>
+    <div id="mathJaxViewer" ref={mathJaxConRef} draggable={true} onDragStart={onDrag}  onMouseDown={onMouseDown}>
 
         <MathJaxContext   renderMode={"pre"} version={3} onError={error} src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js" >
-            <MathJax typesettingOptions={{fn:"tex2svg"}} text={text} dynamic={true} inline onTypeset={imageChanged}> </MathJax>
+            <MathJax typesettingOptions={{fn:"tex2svg"}} text={text} dynamic={true} inline > </MathJax>
        </MathJaxContext>
         <p>test~~~~~~~~~~~~~~~~</p>
 
@@ -100,7 +26,7 @@ const MathJaxViewer: React.FC = () => {
 
         </canvas>
 
-        <button onClick={addClipBoard}>
+        <button onClick={addCanvasToClipboard}>
 test copy
         </button>
     </div>
--- src/direflow-components/math-equation-component/handles/useMathJaxImage.ts
+++ src/direflow-components/math-equation-component/handles/useMathJaxImage.ts
@@ -1,11 +1,135 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {Canvg} from "canvg"
+import * as pngMeta from "@nashiinc/png-metadata/index.js"
+import * as base64 from "base-64";
+
+//need these becuase the version of typescript used here is fairly old
+interface ClipboardItem {
+    readonly types: string[];
+    readonly presentationStyle: "unspecified" | "inline" | "attachment";
+    getType(): Promise<Blob>;
+  }
+  
+  interface ClipboardItemData {
+    [mimeType: string]: any;
+  }
+  
+  declare var ClipboardItem: {
+    prototype: ClipboardItem;
+    new (itemData: ClipboardItemData): ClipboardItem;
+  };
 
 
+  //https://stackoverflow.com/questions/12710001/how-to-convert-uint8-array-to-base64-encoded-string
+const base64_arraybuffer = async (blob:Blob) =>  {
+    const base64url = await new Promise((r) => {
+      const reader = new FileReader()
+      reader.onload = () => r(reader.result)
+      reader.readAsDataURL(blob);
+  })
+
+    return base64url;
+}
+
+
+function drawMathJaxToCanvas(mathJax :HTMLDivElement, canvas : HTMLCanvasElement )
+{
+    let svgCon = mathJax.getElementsByTagName('mjx-container')[0];
+    let removeElement = svgCon.getElementsByTagName("mjx-assistive-mml");
+    if(removeElement.length === 1) svgCon.removeChild(removeElement[0]);
+    let svgData = svgCon.innerHTML;
+
+    let ctx = canvas.getContext('2d');
+    if(ctx)
+    {
+        let v = Canvg.fromString(ctx,svgData)
+        v.start();
+    }
+}
+
+async function  convertCanvasToPng(canvas : HTMLCanvasElement ){
+    return new Promise(resolve  =>{ 
+      canvas.toBlob((blob :any)=>{resolve(blob);}); //converts it to png
+    });
+  }
+
+async function generatePNG(mathJax :HTMLDivElement, canvas : HTMLCanvasElement )
+{
+    drawMathJaxToCanvas(mathJax,canvas);
+
+    //get canvas into png and into Uint8Array
+    let blobb :any = await convertCanvasToPng(canvas);
+    const blobArray = await blobb.arrayBuffer();
+    const blob8 = new Uint8Array(blobArray);
+
+    //get the metadata then 
+    let metaData = pngMeta.readMetadata(blob8)
+    metaData["tEXt"] = {"test":"shit"}
+    let newBlob = pngMeta.writeMetadata(blob8,metaData);
+
+    const backToBlob = new Blob([newBlob],{type: "image/png"});
+    return backToBlob;
+}
 
 
 
 export function useMathJaxImage()
 {
+    let mathJaxConRef = useRef<HTMLDivElement>(null);
+    let canvasRef = useRef<HTMLCanvasElement>(null);
+    const [image,setImage] = useState("");
 
-  return null;
+    
+    async function addCanvasToClipboard()
+    {
+        
+        if(mathJaxConRef?.current && canvasRef?.current){
+            console.log("111111111111111111111")
+            const blob = await generatePNG(mathJaxConRef.current, canvasRef.current);
+            
+            let clipboard :any = navigator.clipboard;
+            clipboard.write([
+              new ClipboardItem({
+                "image/png": blob
+              })
+            ]);   
+        }
+    }
+
+    async function onMouseDown(event : React.MouseEvent){
+      const d = new Date();
+      let ms = d.getMilliseconds();
+      console.log(ms)
+      if(mathJaxConRef?.current && canvasRef?.current){
+        const blob = await generatePNG(mathJaxConRef.current, canvasRef.current);
+        let test  = await base64_arraybuffer(blob) as string;
+        setImage(test);
+      }
+    }
+
+    function onDrag(event : React.DragEvent<HTMLDivElement>) {
+      const d = new Date(); //this seems to slow it down so i can use it 
+      //let ms = d.getMilliseconds();
+      //console.log(ms)
+      //console.log(image)
+      console.log("drag started");
+      console.log("111111111111111111111")
+      var testImage = document.createElement("img");
+      testImage.src = image;
+      var wrapper = document.createElement("div");
+      wrapper.appendChild(testImage);
+      event.dataTransfer.setData("text/html",wrapper.innerHTML.toString());
+      event.dataTransfer.setDragImage(testImage, 0,0)
+    
+    }
+
+    
+
+    return {
+        mathJaxConRef: mathJaxConRef,
+        canvasRef: canvasRef,
+        addCanvasToClipboard: addCanvasToClipboard,
+        onDrag: onDrag,
+        onMouseDown: onMouseDown
+    };
 }
